b7ed8efca987a5cd724477721ebc06633df9c30f
Suryakant Tyagi <suryakant28.st@gmail.com>
2023-07-25 23:08:50 +0530

    initiated service card and some css modifications

---
+++ src/components/card-service/cardService.js
@@ -0,0 +1,12 @@
+import './cardService.scss';
+
+function CardService() {
+    return (
+        <div className="cardService">
+            <div className='card-head'></div>
+            <div className='card-body'></div>
+        </div>
+    )
+}
+
+export default CardService;
